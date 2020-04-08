4bb09090ab73a3bc237a07acc9b7618304351e83
Rwanda S <rwanda.ace@gmail.com>
2020-04-08 19:36:57 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -20,19 +20,20 @@ attr_accessor :name, :yrs_experience
   meals.map do |meal|
     meal.waiter
   end
-  def 
+end
+   
   def meals
   Meal.all.select do |meal|
     meal.waiter == self #checking for waiter now
   end
+end
   def best_tipper
   best_tipped_meal = meals.max do |meal_a, meal_b|
     meal_a.tip <=> meal_b.tip
   end
   best_tipped_meal.customer
   end
-end
-end
+
   
   
 end
