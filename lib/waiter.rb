a896d8042b1be25a032658f621ec771e64bc317d
Rwanda S <rwanda.ace@gmail.com>
2020-04-08 19:05:21 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -12,4 +12,9 @@ enattr_accessor :name, :yrs_experience
  
   def self.all
     @@all
-  endd
+  end
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+  
+end
